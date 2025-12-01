c4870f01e2c1289c781a8afbb2131eb64730145c
tvnetx01 <tvnetx01@gmail.com>
2025-12-02 02:40:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,13 @@
 {
   "groups": {
-    "bsl2x": [ "indosiarL13", "rb12", "m2", "ms12"],
-    "bsl1": ["indosiarL1", "indosiarL12", "rb12", "m1", "ms11"],
-   "bsl": ["vidl1", "vidl12", "indosiarL1", "indosiarL12", "rb11", "m1", "ms11"],
-
-     "timnas": ["dazn1es", "soccerch", "rctihd","rb15" ,"tg15"],
-
-      "ucl9": ["vvip", "soccerch", "go3x15","rb15" ,"tg15"],
-      
-      "wc17": ["universo", "tsn3","tsn2" ,"fs2", "fifa1+", "fifa2+", "fifa3+", "fifa4+"],
-     
-   
-    "epl1": ["ap3sr", "fubos1", "tnt1uk", "psn2", "telemundo", "go3x6", "astorgs", "m5"],
-    "epl2": ["espn6bo", "psn3", "tntmx", "telemundo", "go3x7", "m5"],
-    "epl3": ["espn7bo", "psn4", "go3x8"],
-    "epl4": ["ap3sr", "as1sr", "psn1", "go3x9"],
-    "epl5": ["ap3sr", "fubos1", "telemundo", "psn2", "go3x10", "astrogs", "m5"],
-    "epl6": ["espn7bo", "go3x6"],
-    "epl": ["vvip", "fubos1", "ap3sr", "psn2" ,"bein4vp", "go3x10", "astrogs", "m5"],
-      
-    "laliga1": [ "bein1uk", "bein1my", "bein1vp", "bein1sg", "ap1sr", "zsport2",  "lltv", "dazn1de","skyllmx", "m10", "ms10"],
-    "laliga2": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "dazn1de", "zsport1", "skyllmx", "lltv", "m10", "ms10"],
-    "laliga3": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "dazn1de", "zsport1", "m10", "ms10", "skyllmx", "lltv"],
-    "laliga4": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "skyllmx", "lltv", "zsport2", "m10", "ms10"],
-    "laliga5": ["vvip", "bein2uk", "bein3vp", "bein3sg", "zsport2", "ap2sr", "zsport1", "skyllmx", "lltv", "m10", "ms10"],
     
-    "seriea1": ["bein3uk", "astrob1", "zsport3", "ms7", "m7"],
-    "seriea2": ["bein3uk", "bein4my", "tnt5uk", "espnmx", "astrof", "zsport2", "ms7", "m7" ],
-    "seriea3": ["bein3uk", "tnt5uk", "bein4my", "espnmx", "zsport2",  "astrob1", "ms7", "m7"],
-    "seriea4": ["bein3uk", "tnt1uk", "bein4my", "tnt5uk", "espnmx", "zsport1", "astrob1", "ms7", "m7"],
-    "seriea5": ["vvip", "bein3uk", "tnt1uk", "bein4my","go3s2","astrof", "ms7", "m7"],
-    "seriea6": ["vvip", "espn7bo", "astrob1", "zsport1", "bein4my"],
-
-      
-    "bundesliga1": ["soccerch", "bein1uk", "psn5", "skyblmx", "ms9", "m9"],
-    "bundesliga2": ["soccerch", "psn4", "skyblmx", "ms9", "m9"],
-    "bundesliga3": ["bein1uk",  "skyblmx",  "soccerch", "psn5", "ms9", "m9"],
-    "bundesliga4": ["ss1", "rb9", "tg9"],
-    "bundesliga5": ["soccerch", "bein1uk", "psn5", "rctihd", "skyblmx", "ms9", "m9"],
-    "bundesliga6": ["rctihd", "soccerch", "ms9", "m9"],
-      
-    "ligue11": ["5il", "ms8" , "m8"],
-    "ligue112": ["rb5", "m8"],
-    "ligue13": ["rb6", "tg6"],
-    "ligue14": ["rb7", "tg7"],
-    "ligue15": ["5star", "ms8" , "m8"],
-      
-    "carabao1": ["vvip","astrogs", "espnbo", "hubs1", "rb9", "tg9"],
-    "carabao2": ["vvip","bein2uk", "espn5bo" ,"ap3sr", "hubs2", "astrof"],
-    "carabao3": ["vvip","espn3mx", "espn2bo" ,"hubs3", "rb10", "tg10"],
-    "carabao4": ["vvip","espn3bo", "hubs4", "rb11", "tg11"],
-    "carabao5": ["vvip", "bein3uk", "espn4bo"],
-      
-    "dfbp1": ["rb12", "tg12"],
-    "dfbp2": ["espn2mx","astrogs", "novas1", "bein3my"],
-    "dfbp3": ["rb13", "tg13"],
-    "dfbp4": ["rb14","tg14"],
-    "dfbp5": ["vvip", "espn2mx", "bein3my", "zsport1"],
-
+    "eropa": [ "epl", "laliga", "seriea", "bundesliga", "ligue1", "portugal",  "belgia", "ucl","uel", "uecl", "carabao", "facup"],
+    "asia": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "dazn1de", "zsport1", "skyllmx", "lltv", "m10", "ms10"],
+    "indonesia": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "dazn1de", "zsport1", "m10", "ms10", "skyllmx", "lltv"],
+    "tvnasional": [ "bein2uk", "ap1sr", "bein2vp", "bein2sg", "skyllmx", "lltv", "zsport2", "m10", "ms10"],
+    "tvsports": ["vvip", "bein2uk", "bein3vp", "bein3sg", "zsport2", "ap2sr", "zsport1", "skyllmx", "lltv", "m10", "ms10"],
     
-    "ucl1": ["tnt3uk", "as5cr", "zsport5"],
-    "ucl2": ["skye1", "psn5", "zsport3"],
-    "ucl3": ["bein2sg", "bein2vp", "as2cr", "psn1"],
-    "ucl4": ["skye2", "rb4", "tg4"],
-    "ucl5": ["skye3", "as6cr"],
-    "ucl6": ["tnt1uk", "bein1sg", "bein1vp", "psn2", "bein3my"],
-    "ucl7": ["skye4", "rb5", "tg5"],
-    "ucl8": ["skye5", "rb6", "tg6"],
-    "ucl9": ["skye6", "as3cr", "psn4", "zsport1"],
-   
-      
-    "ucel1": ["espnbo","rb7", "tg7"],
-    "ucel2": ["espn2bo", "rb8", "tg8"],
-    "ucel3": ["espn3bo","rb6", "tg6", "zsport1"],
-    "ucel4": ["tnt2uk", "bein3sg", "bein3vp"],
-    "uel6x": ["vvip", "tnt2uk", "bein1sg", "bein1vp", "bein1my"],
-    "eredivisi1": ["espn4bo", "as5cr", "rb8","tg8"],
-    "eredivisi2": ["espn5bo", "rb9", "tg9"],
-    "eredivisi3": ["espn6bo", "rb10", "tg10"],
-     "eredivisi4": ["espn7bo", "rb11", "tg11"],
-    "eredivisi5": ["bein5vp", "rb12", "tg12"], 
-    "uel3": ["espnbo", "rb13", "tg13"],
-   
-   "uel1": ["fifa+fhd", "fifa+hd", "fifa+sd", "rb14", "tg14"],
-    "uel2": ["universo", "rb1", "tg1"],
-    "uel3": ["rb2","tg2"],
-    "uel4": ["bein4my", "rb3", "tg3"],
-     "uel5": ["rb4","tg4"],
-    "uel6": ["bein4my", "rb6", "tg6"],
-  
-  "bwf": ["court1", "court2","court3", "bwf1", "bwf2", "bwf3"]},
+    "tvmovies": ["court1", "court2","court3", "bwf1", "bwf2", "bwf3"]},
 
 
 
